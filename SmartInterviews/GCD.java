fe9e900d71bb51a89cad7cdd582a47513c8f5410
Charan Teja Chukkala <149792997+charanteja-7@users.noreply.github.com>
2024-03-27 00:14:05 +0530

    Create GCD.java

---
+++ SmartInterviews/GCD.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static int gcd(int a, int b){
+        if(a<=0){
+            return b;
+        }
+        if(a<b){
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        return gcd(a-b,b);
+    }
+	public static void main(String[] args) {
+		System.out.println(gcd(24,36));
+	}
+}
